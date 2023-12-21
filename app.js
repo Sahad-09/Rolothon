b74e8599bfd63d127f518dbc2ff16275ff0e1fca
Sahad <msahad124@gmail.com>
2023-12-21 14:32:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors');
+const app = express()
+const { db } = require('./db/db');
+const { readdirSync } = require('fs')
+
+const PORT = process.env.PORT
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send("Hello world")
+})
+
+//Routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+
+
+const server = () => {
+    db()
+    app.listen(PORT, () => {
+        console.log('listening to port:', PORT)
+    })
+}
+
+server()
